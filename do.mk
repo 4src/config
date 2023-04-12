a94dfc303a0cf2a12a4795652ebd41b8197f49f2
Tim Menzies <timm@users.noreply.github.com>
2023-04-12 09:22:30 -0400

    saving

--- do.mk
+++ do.mk
@@ -5,7 +5,7 @@ DO_yellow=\033[93m
 DO_white=\033[0m
 ##########################################################
 define DO
-   printf "\033[1;33mTEXT\033[00m"; figlet -W -f straight  ${1}s; printf "$$DO_white"
+   printf "\033[1;33m"; figlet -W -f straight  ${1}s; printf "$$DO_white"
 	 $(foreach d,$(DO_repos), printf "$(DO_cyan):: $1 $d$(DO_white)\n"; cd $d; $(MAKE) $1;)
 endef
 
